fe075c4ad090e84bb7d265e34b54293220dc27b4
mukul rawat <rawatmukul86@gmail.com>
2015-09-24 19:34:56 +0530

    Finished the Levenshtein function and its test

--- bktree/levenshtein.go
+++ bktree/levenshtein.go
@@ -3,50 +3,50 @@ package bktree
 // The function find Levenshtein distance between two strings
 // It takes as argument two strings and returns an integer value
 // which is the levenshtein distance between the two strings
-func Levenshtein(a, b string) int{
+func levenshtein(a, b string) int {
 
-    // Find the length of the strings
-    la, lb := len(a), len(b)
+	// Find the length of the strings
+	la, lb := len(a), len(b)
 
-    // Create array to store distance
-    col := make([]int, la+1)
+	// Create array to store distance
+	col := make([]int, la+1)
 
-    var lastdiag, olddiag, i, j int
-    var temp1, temp2, temp3 int
+	var lastdiag, olddiag, i, j int
+	var temp1, temp2, temp3 int
 
-    for i = 1; i <= la; i++{
-        col[i] = i
-    }
+	for i = 1; i <= la; i++ {
+		col[i] = i
+	}
 
-    for i = 1; i<= lb; i++{
-        col[0] = i
+	for i = 1; i <= lb; i++ {
+		col[0] = i
 
-        for j, lastdiag = 1, i-1; j<=la; j++{
-            olddiag = col[j]
-            temp1 = col[j] + 1
-            temp2 = col[j-1] + 1
-            if a[j-1] == b[i-1]{
-                temp3 = lastdiag + 0
-            }else{
-                temp3 = lastdiag + 1
-            }
+		for j, lastdiag = 1, i-1; j <= la; j++ {
+			olddiag = col[j]
+			temp1 = col[j] + 1
+			temp2 = col[j-1] + 1
+			if a[j-1] == b[i-1] {
+				temp3 = lastdiag + 0
+			} else {
+				temp3 = lastdiag + 1
+			}
 
-            col[j] = min(temp1, temp2, temp3)
-            lastdiag = olddiag
-        }
-    }
+			col[j] = min(temp1, temp2, temp3)
+			lastdiag = olddiag
+		}
+	}
 
-    return col[la]
+	return col[la]
 }
 
-// A function to find the minimum of two integers
-// Returns the minimum of the two integers
+// A function to find the minimum of three integers
+// Returns the minimum of the three integers
 func min(a, b, c int) int {
-    if a < b && a < c{
-        return a
-    }else if b < a && b <c{
-        return b
-    }else{
-        return c
-    }
+	if a < b && a < c {
+		return a
+	} else if b < a && b < c {
+		return b
+	} else {
+		return c
+	}
 }
